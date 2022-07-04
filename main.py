a23c512649377d2c48fa6ea35d648dcacfa030d8
alebreuilly <alebreuilly@gmail.com>
2022-07-04 11:28:01 +0100

    remove old code related to exporting interview data.

--- main.py
+++ main.py
@@ -33,56 +33,11 @@ df.to_csv('meta.csv', index=False)
 sss_file = r'C:\training data.sss'
 asc_file = r'C:\Askia Training Data.asc'
 
-
 conn = connector_sss.Connection(sss_file=sss_file, asc_file=asc_file)
 survey_from_sss = connector_sss.Survey(connection=conn)
 
-
-# sss.survey = sss.get_survey()
-
-
-# with open(f_data) as f:
-#     lines = f.readlines()
-#     count_row_id = 0
-#     interviews = []
-#
-#     for l in lines:
-#         # create Interview
-#         count_row_id += 1
-#         i = {'ident': count_row_id,
-#              'date': None,
-#              'complete': True,
-#              'dataItems': []
-#              }
-#         iv = ts.tsapi.Interview(**i)
-#
-#         # populate dataitems
-#         for loc in ss.meta_data.variable_positions:
-#             start = 0
-#             finish = 0
-#             subfields = 0
-#             width = 0
-#
-#             start = int(loc['start'])-1
-#             finish = int(loc['finish'])
-#             if 'subfields' in loc.keys():
-#                 subfields = int(loc['subfields'])
-#             else:
-#                 subfields = 0
-#             if 'width' in loc.keys():
-#                 width = int(loc['width'])
-#             else:
-#                 width = 0
-#             datapoint = l[start:finish]
-#             datapoints = []
-#             if subfields > 0:
-#                 datapoints = [datapoint[i:i + width] for i in range(0, len(datapoint), width)]
-#             else:
-#                 datapoints.append(datapoint)
-#
-#             di = ts.tsapi.DataItem(ident=loc['ident'], values=datapoints)
-#             iv.data_items.append(di)
-#         interviews.append(iv)
-
 with open('data.json', 'w', encoding='utf8') as f:
-    json.dump(sss.survey.to_tsapi(), f, indent=4, ensure_ascii=False)
+    json.dump(survey_from_sss.survey.to_tsapi(),
+              f,
+              indent=4,
+              ensure_ascii=False)
